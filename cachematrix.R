1d0a27b532fc3a5b492b06856ae97182c1fd956f
Ignacio Rodriguez <ignrodri@pdi.ucm.es>
2014-05-26 00:44:40 +0200

    Ready for grading

--- cachematrix.R
+++ cachematrix.R
@@ -3,10 +3,18 @@
 ## Please see https://www.coursera.org/course/rprog
 
 ## This function creates an object from a matrix
-## set: sets the matrix which is inverted
-## get: gets the matrix which is inverted
+## set: sets the original matrix
+## get: gets the original matrix
 ## setinv: sets the inverted matrix
 ## getinv: gets the inverted matrix
+
+## Warning: the setinv function will set the inverse irrespective on whether the result is correct or not
+
+## To clear the cache, get and set the original matrix, as in this example
+## m <- makeCacheMatrix(mat)
+## x1 <- cacheSolve(m)
+## m$set(m$get())
+## x1 <- cacheSolve(m)
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
@@ -28,6 +36,9 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Example:
 ## m <- makeCacheMatrix(mat)
 ## invmat <- cacheSolve(m)
+
+## NB: Additional arguments passed to cacheSolve are passed on to solve
+## These additional arguments are ignored if an inverted matrix has already been precomputed 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
